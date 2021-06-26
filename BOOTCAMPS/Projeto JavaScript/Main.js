35a797f7127647ecea7fbb9df646faea0738572b
VCN09 <vcn09ti@gmail.com>
2021-06-25 21:31:05 -0300

    git commit

---
+++ BOOTCAMPS/Projeto JavaScript/Main.js
@@ -0,0 +1,128 @@
+
+
+
+
+
+let line = gets().split(" ");
+let A = parseInt(line[0]);
+let B = parseInt(line[1]);
+let total = 0; // Altere o valor da variável com o cálculo esperado
+console.log("X = " + total);
+
+
+
+
+
+
+
+
+
+/*
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+}
+function redirecionar(){
+        window.open("https://www.ia-shooters.net");
+}
+   // console.log(document.getElementById("Agradecimento"));
+function trocar(){
+    document.getElementById
+}
+
+*/
+
+/* function botao(){
+    alert("Obrigado por clicar");
+}
+
+*/
+
+
+
+/*function validaIdade(idade)
+
+
+
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+
+alert(soma(5,10))
+*/
+
+
+/*function soma(n1, n2){
+    return n1 + n2
+}
+*/
+
+
+/*var count;
+for (count = 0; count <= 5; count++){
+    alert(count);
+}
+*/
+
+
+
+
+/*
+var count = 0;
+while (count < 5) {
+    console.log (count);
+    count = count + 1;     // o count ++ é a mesma coisa que count +1
+};
+*/
+
+/*var idade = prompt("Qual a sua idade? ");
+//var idade = 18;
+if (idade >= 18) {
+    alert("maior de idade"); 
+}
+else {
+    alert( "menor de idade");
+};*/
+
+
+
+
+
+
+
+
+
+
+
+
+/*var fruta = {nome: "maça", cor: "vermelha", quantidade: 2};
+console.log(fruta.nome);
+console.log(fruta.cor);
+console.log(fruta.quantidade);*/
+
+
+
+/*var lista = ["maça", "pêra", "laranja"];
+lista.push("uva");
+//lista pop();
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" | "));*/
+
+
+
+
+
+// var idade = 39;
+//var idade2 = 10;
+//alert (" BEM VINDO " + nome);
+//var nome = "VCN09";
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "Japão é o melhor time do mundo";
+// alert(nome + "tem" + idade + "anos");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.toLowerCase());
+//alert(frase.replace("Japão", "Brasil"));
